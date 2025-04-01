de2fa2e1ca5a0952c6aebf423b83d69e0b2a5bb1
yacodev <cyaco33@gmail.com>
2025-04-01 17:05:12 -0500

    update skills and workexperience

--- src/components/sections/Experiences.js
+++ src/components/sections/Experiences.js
@@ -1,95 +1,108 @@
-import Pagetitle from '../elements/Pagetitle';
-import Timeline from '../elements/Timeline';
+import Pagetitle from "../elements/Pagetitle";
+import Timeline from "../elements/Timeline";
 
 const experienceData = [
   {
     id: 1,
-    title: 'Desarrollador Front-end Sr.',
-    years: 'Abril 2024 – Actualidad',
-    content: 'Indra - Rimac Seguros',
+    title: "Desarrollador Front-end Sr.",
+    years: "Abril 2024 – Febrero 2025",
+    content: "Indra - Rimac Seguros",
     description: [
-      'Desarrollo de una aplicación web para corredores para la emisión de nuevos seguros (TREC, CAR, eventos).',
-      'Desarrollo del nuevo proyecto SLIPs para la emisión de contratos de seguros.',
-      'Tecnologías implementadas Sass, Javascript, React, Typescript, Redux, Zustand y Micro-frontends.',
+      "Desarrollo de una aplicación web  para corredores, para la optimización de la emisión de nuevos seguros (Proyecto finalizado).",
+      "Implementación de micro-frontends y optimización del rendimiento.",
+      "Creación de pruebas unitarias con Jest para garantizar la calidad del código.",
     ],
+    achievements: [" Entrega del proyecto en el tiempo establecido."],
+    technologies:
+      "Tailwind, Sass, Javascript, React, Typescript, Redux, Zustand,Micro-frontends, Jest, AWS",
   },
   {
     id: 2,
-    title: 'Desarrollador Front-end',
-    years: 'Enero 2022 – Diciembre 2023',
-    content: 'Startup DEUNA: Libera el poder de tu  e-commerce',
+    title: "Desarrollador Front-end",
+    years: "Enero 2022 – Diciembre 2023",
+    content: "DEUNA: Libera el poder de tu  e-commerce (Startup )",
     description: [
-      'Desarrollo del admin web para que puedas administrar tu tienda virtual.',
-      'Implementación de testing con Jest y  testing E2E con Cypress',
-      'Implementación de eventos.',
-      'Documentación de todos los productos de DEUNA.',
-      'Tecnologías implementadas: React , Redux, Typescript, Jest y Cypress',
+      "Desarrollo de la plataforma de administración para que los comercios gestionen sus ordenes de compra, pasarelas de pago, promociones y clientes, tanto la versión 1 y 2.",
+      "Implementar tanto las pruebas unitarias y pruebas E2E.",
+      "Implementación de Storybook para documentación de componentes reutilizables.",
+      "Documentación y desarrollo de ejemplos de implementación del: SDK, payment-widget, de las API de DEUNA.",
     ],
+    achievements: [
+      "Generar una cultura de testing en el equipo,pasando de no tener cobertura a tener un mínimo del 70% de cobertura.",
+    ],
+    technologies:
+      "Material UI, Bootstrap, Javascript, React, Typescript,  NextJs, Storybook, Redux, styled components,  pruebas unitarias con Jest y Cypress, AWS",
   },
   {
     id: 3,
-    title: 'Desarrollador Front-end',
-    years: 'Enero 2021 – Diciembre 2021',
-    content: 'Construtec: Software para la construcción.',
+    title: "Desarrollador Fullstack enfocado en Frontend",
+    years: "Enero 2021 – Diciembre 2021",
+    content: "Construtec: Software para la construcción. (Startup)",
     description: [
-      'Lideré al equipo de desarrolladores frontend durante todo el proceso de desarrollo del producto.',
-      'Desarrolle la plataforma de Construtec para la gestión de almacenes y valorizaciones de obra (https://appconstrutec.com).',
-      'Diseñe el modelo de negocio para satisfacer una necesidad del mercado, estuve también encargado de la comercialización y lanzamiento del producto.',
-      'Tecnologias implementadas: HTML, CSS, React , Typescript y Cypress.',
+      "Liderar al equipo de frontend de la empresa.",
+      "Desarrollo de la plataforma web para la gestión de almacenes y valorizaciones de obra.",
+      "Colaborar para desarrollo del backend con el modulo gestión de almacenes.",
     ],
+    achievements: ["Ganamos Startup Perú 9G"],
+    technologies:
+      "Material UI, Javascript, Angular, React, Typescript, Next.js, Context API, Node.js, Nest.js, PostgreSQL, Docker, AWS",
   },
   {
     id: 4,
-    title: 'Desarrollador Full stack',
-    years: 'Enero 2020 – Diciembre 2020',
-    content: 'Smart Cities Peru: Software factory',
+    title: "Desarrollador Full stack",
+    years: "Enero 2020 – Diciembre 2020",
+    content: "Smart Cities Peru: Software factory",
     description: [
-      'Desarrollo de una aplicación web para brindar capacitación al personal de las municipalidades de toda la región Cusco (http://lacasadelatm.com).',
-      'Dirigir al equipo de Desarrolladores utilizando la metodología SCRUM.',
-      'Desarrollamos una plataforma web para realizar el seguimiento a la ejecución de Proyectos de Inversión Pública para el Gobierno Regional del Cusco.',
-      'Tecnologías empleadas : React con Redux y para el backend Node.js con express.js.',
-      'Tecnologias implementadas Sass, React y Typescript.',
+      "Desarrollo de una aplicación web para brindar capacitación al personal de las municipalidades de toda la región Cusco (http://lacasadelatm.com).",
+      "Desarrollamos una plataforma web para realizar el seguimiento a la ejecución de Proyectos de Inversión Pública para el Gobierno Regional del Cusco.",
     ],
+    achievements: [
+      "Lograr un 90% de satisfacción del cliente con el producto final.",
+    ],
+    technologies:
+      "Javascript, React, React Native, Angular, Typescript, Node.js, Nest.js MySQL, Dockers.",
   },
   {
     id: 5,
-    title: 'Product manager - Product owner ',
-    years: 'Enero 2016 – Agosto 2017',
-    content: 'Construinnova Perú',
+    title: "Product owner ",
+    years: "Enero 2016 – Agosto 2017",
+    content: "Construinnova Perú",
     description: [
-      'Desarrollamos un software de escritorio para el control de proyectos de inversión  pública (CPIP).',
-      'Diseño del Producto e implementación de modelos de negocio.',
-      'Desarrolle la website del CPIP',
-      'Lideré a un equipo de tres ingenieros durante el proceso de desarrollo del software CPIP usando Java.',
+      "Desarrollamos un software de escritorio para el control de proyectos de inversión  pública (CPIP).",
+      "Diseño del Producto e implementación de modelos de negocio.",
+      "Desarrolle la website del CPIP",
+      "Lideré a un equipo de tres ingenieros durante el proceso de desarrollo del software CPIP usando Java.",
     ],
+    technologies: "SCRUM, Jira, Github",
   },
   {
     id: 6,
-    title: 'Cofundador - CEO',
-    years: 'Enero 2013 – Diciembre 2014',
-    content: 'Evertech.',
+    title: "Cofundador - CEO",
+    years: "Enero 2013 – Diciembre 2014",
+    content: "Evertech.",
     description: [
-      'Cofunde la startup Evertech',
-      'Desarrollamos un producto en  hardaware basado en la rasberry pi para hacer publicidad en vehiculos de transporte público.',
-      'Desarrolle la landing page de  Evertech, las campañas de publicidad y  encargado de las ventas.',
-      'Diseñe el modelo de negocio.',
+      "Cofunde la startup Evertech",
+      "Desarrollamos un producto en  hardaware basado en la rasberry pi para hacer publicidad en vehiculos de transporte público.",
+      "Desarrolle la landing page de  Evertech, las campañas de publicidad y  encargado de las ventas.",
+      "Diseñe el modelo de negocio.",
     ],
+    technologies: "Python, Rasberry pi ",
   },
 ];
 
 function Experiences() {
   return (
-    <section id='experience'>
-      <div className='container'>
-        <Pagetitle title='Experiencia como Desarrollador:' />
-        <div className='row'>
-          <div className='col-md-12'>
-            <div className='spacer d-md-none d-lg-none' data-height='30'></div>
-            <div className='timeline exp bg-white rounded shadow-dark padding-30 overflow-hidden'>
+    <section id="experience">
+      <div className="container">
+        <Pagetitle title="Experiencia como Desarrollador:" />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="spacer d-md-none d-lg-none" data-height="30"></div>
+            <div className="timeline exp bg-white rounded shadow-dark padding-30 overflow-hidden">
               {experienceData.map((experience) => (
                 <Timeline key={experience.id} education={experience} />
               ))}
-              <span className='line'></span>
+              <span className="line"></span>
             </div>
           </div>
         </div>
--- src/components/sections/Skills.js
+++ src/components/sections/Skills.js
@@ -1,35 +1,106 @@
-import Pagetitle from '../elements/Pagetitle';
+import Pagetitle from "../elements/Pagetitle";
 const skillsFronted = [
   {
     id: 1,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+    name: "HTML",
   },
   {
     id: 2,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+    name: "CSS",
   },
   {
     id: 3,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "Javascript",
   },
   {
     id: 4,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    name: "React",
   },
   {
     id: 5,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/angularjs/angularjs-original.svg",
+    name: "Angular",
   },
 
   {
     id: 6,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    id: 7,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "Next",
+  },
+  {
+    id: 8,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+    name: "Redux",
+  },
+  {
+    id: 9,
+    image:
+      "https://user-images.githubusercontent.com/958486/218346783-72be5ae3-b953-4dd7-b239-788a882fdad6.svg",
+    name: "Zustand",
+  },
+  {
+    id: 10,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/jest/jest-plain.svg",
+    name: "Jest",
+  },
+  {
+    id: 11,
+    image: "https://www.cypress.io/_astro/cypress-logo.D87396b0.svg",
+    name: "Cypress",
+  },
+  {
+    id: 12,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+    name: "Tailwind",
+  },
+  {
+    id: 13,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    id: 14,
+    image: "https://vite.dev/logo.svg",
+    name: "Vite",
+  },
+  {
+    id: 15,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/webpack/webpack-original.svg",
+    name: "Webpack",
+  },
+  {
+    id: 16,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+    name: "Sass",
+  },
+
+  {
+    id: 17,
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
   },
 ];
 
@@ -37,64 +108,80 @@ const skillsBackend = [
   {
     id: 1,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+    name: "Node",
   },
   {
     id: 2,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+    name: "Express",
   },
   {
     id: 3,
-    image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/rails/rails-original-wordmark.svg',
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a8/NestJS.svg",
+    name: "Nest",
   },
   {
     id: 4,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+    name: "MySQL",
   },
   {
     id: 5,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+    name: "MongoDB",
   },
   {
     id: 6,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+    name: "PostgreSQL",
   },
   {
     id: 7,
     image:
-      'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg',
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+    name: "Docker",
+  },
+  {
+    id: 8,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg",
+    name: "AWS",
   },
 ];
 
 function Skills() {
   const renderSkills = (skills) => (
-    <div className='row'>
+    <div className="row">
       {skills.map((skill) => (
         <div
-          className='col-md-1 col-3'
+          className="col-md-1 col-3 "
           key={skill.id}
-          style={{ marginBottom: '15px', marginLeft: '15px' }}
+          style={{ marginBottom: "15px", marginLeft: "15px", gap: "10px" }}
         >
-          <div className='client-item'>
+          <div className="client-item">
             <div
-              className='inner'
+              className="inner"
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                width: '100%',
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
               }}
             >
               <img
                 src={skill.image}
-                alt='skill-name'
-                style={{ width: '60px', height: '60px' }}
+                alt="skill-name"
+                style={{ width: "60px", height: "60px" }}
               />
             </div>
+          </div>
+
+          <div className="client-item">
+            <span>{skill?.name}</span>
           </div>
         </div>
       ))}
@@ -102,18 +189,18 @@ function Skills() {
   );
 
   return (
-    <div id='skills'>
-      <div className='container'>
-        <Pagetitle title='Herramientas de desarrollo:' />
-        <div className='mt-3'>
+    <div id="skills">
+      <div className="container">
+        <Pagetitle title="Tecnologias:" />
+        <div className="mt-3">
           <p>Frontend:</p>
-          <div className='rounded bg-white shadow-dark p-3 '>
+          <div className="rounded bg-white shadow-dark p-3 ">
             {renderSkills(skillsFronted)}
           </div>
         </div>
-        <div className='mt-3'>
+        <div className="mt-3">
           <p>Backend:</p>
-          <div className='rounded bg-white shadow-dark p-3'>
+          <div className="rounded bg-white shadow-dark p-3">
             {renderSkills(skillsBackend)}
           </div>
         </div>
